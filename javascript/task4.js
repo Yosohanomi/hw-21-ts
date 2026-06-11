36ce02fe2c71ebea3f2d3462d74157187c797bb8
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2026-06-11 23:40:52 +0300

    beninging

---
+++ javascript/task4.js
@@ -0,0 +1,9 @@
+"use strict";
+// Створити функцію, яка дозволяє знайти
+//  або останню цифру числа, або останній символ числа.
+// function getLast(input) {
+//     let str = String(input);
+//     return str.at(-1); 
+//   }
+//   console.log(getLast(12345));    
+//   console.log(getLast("Hello"));  
